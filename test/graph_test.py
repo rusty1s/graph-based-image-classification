ab351114cc3f839bc602d29bd967a69d0713dfe7
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-18 17:47:32 +0100

    finished graph test

--- test/graph_test.py
+++ test/graph_test.py
@@ -1,16 +1,41 @@
 from __future__ import print_function
 
 from nose.tools import *
-from pynauty import Graph
+from pynauty import Graph, certificate
 
 
 def test_graph():
-    g = Graph(2)
-    g.connect_vertex(0, [1])
-    g.connect_vertex(1, [0])
+    # build the graph
+    g = Graph(10)
 
-    assert_equal(g.number_of_vertices, 2)
+    g.connect_vertex(0, [1, 2, 3, 4, 5])
+    g.connect_vertex(1, [0, 4, 5])
+    g.connect_vertex(2, [0, 3])
+    g.connect_vertex(3, [0, 2, 4, 6, 7])
+    g.connect_vertex(4, [0, 1, 3, 5, 6, 8, 9])
+    g.connect_vertex(5, [0, 1, 4])
+    g.connect_vertex(6, [3, 4, 7, 8])
+    g.connect_vertex(7, [3, 6, 8])
+    g.connect_vertex(8, [4, 6, 7, 9])
+    g.connect_vertex(9, [4, 8])
+
+    g.set_vertex_coloring([set([0]), set([1, 2, 3, 4, 5]), set([6, 7, 8, 9])])
+
+    # test the graph
+    assert_equal(g.number_of_vertices, 10)
     assert_equal(g.directed, False)
-    assert_equal(g.adjacency_dict[0], [1])
-    assert_equal(g.adjacency_dict[1], [0])
-    assert_equal(g.vertex_coloring, [])
+
+    assert_equal(g.adjacency_dict[0], [1, 2, 3, 4, 5])
+    assert_equal(g.adjacency_dict[1], [0, 4, 5])
+    assert_equal(g.adjacency_dict[2], [0, 3])
+    assert_equal(g.adjacency_dict[3], [0, 2, 4, 6, 7])
+    assert_equal(g.adjacency_dict[4], [0, 1, 3, 5, 6, 8, 9])
+    assert_equal(g.adjacency_dict[5], [0, 1, 4])
+    assert_equal(g.adjacency_dict[6], [3, 4, 7, 8])
+    assert_equal(g.adjacency_dict[7], [3, 6, 8])
+    assert_equal(g.adjacency_dict[8], [4, 6, 7, 9])
+    assert_equal(g.adjacency_dict[9], [4, 8])
+
+    assert_equal(g.vertex_coloring[0], set([0]))
+    assert_equal(g.vertex_coloring[1], set([1, 2, 3, 4, 5]))
+    assert_equal(g.vertex_coloring[2], set([6, 7, 8, 9]))
