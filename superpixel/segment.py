e79edf4006b3b1efdebadc246ec9637394cba9d9
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-20 16:17:26 +0100

    pep8 test should be running

--- superpixel/segment.py
+++ superpixel/segment.py
@@ -1,8 +1,12 @@
 class Segment(object):
-    def __init__(self, index, top_left, pixel, mean, neighbors):
+    def __init__(self, index, top_left, shape, mean, neighbors):
         self.index = index
         self.start = start
         self.shape = shape
         self.neighbors = neighbors
         self.mean = (0, 0, 0)
         self.center = (0, 0)
+
+    def center(self):
+        pass
+        # run through each y shape and  1
