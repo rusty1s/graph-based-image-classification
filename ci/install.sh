c530f017bc7b30425722ae28d7fa4876d30f9ece
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-20 16:38:09 +0100

    source instead of bash

--- ci/install.sh
+++ ci/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bash ci/conda.sh -v $TRAVIS_PYTHON_VERSION -n test --tensorflow $TENSORFLOW --nauty $NAUTY --pynauty $PYNAUTY
+source ci/conda.sh -v $TRAVIS_PYTHON_VERSION -n test --tensorflow $TENSORFLOW --nauty $NAUTY --pynauty $PYNAUTY
