93c9fd17ee4d707e0a4b54447fcb16b1042c892c
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-20 20:24:42 +0100

    added segment center

--- superpixel/segment.py
+++ superpixel/segment.py
@@ -1,28 +1,32 @@
 import numpy as np
+from scipy import ndimage
 
 
 class Segment(object):
-    def __init__(self, index, left=None, top=None, width=1, height=1):
+    def __init__(self, index):
         self.index = index
-        self.left = left
-        self.top = top
-        self.width = width
-        self.height = height
 
-        # compute later
+        self.left = None
+        self.top = None
+
+        # initialize with 0, so we have an easier time calculating th values
+        self.width = 0
+        self.height = 0
+
         self.mask = None
+        self.count = None
         self.mean = None
         self.center = None
         self.neighbors = None
 
     @staticmethod
-    def generate(superpixel):
-        superpixel = np.array(superpixel)
-        segment_values = np.unique(superpixel)
+    def generate(superpixels):
+        superpixels = np.array(superpixels)
+        segment_values = np.unique(superpixels)
         segments = {i: Segment(i) for i in segment_values}
 
         # compute top/left
-        for y, row in enumerate(superpixel):
+        for y, row in enumerate(superpixels):
             for x, v in enumerate(row):
                 s = segments[v]
 
@@ -30,17 +34,19 @@ class Segment(object):
                 s.top = y if s.top is None else min(s.top, y)
 
         # compute width/height
-        for y, row in enumerate(superpixel):
+        for y, row in enumerate(superpixels):
             for x, v in enumerate(row):
                 s = segments[v]
 
                 s.width = max(s.width, x + 1 - s.left)
                 s.height = max(s.height, y + 1 - s.top)
 
+        # compute mask and center
         for i in segments:
             s = segments[i]
-            sliced = superpixel[s.top:s.top+s.height, s.left:s.left+s.width]
+            sliced = superpixels[s.top:s.top+s.height, s.left:s.left+s.width]
             s.mask = np.zeros(sliced.shape, dtype=np.uint8)
             s.mask[sliced == s.index] = 255
+            s.center = ndimage.measurements.center_of_mass(s.mask)
 
         return segments
