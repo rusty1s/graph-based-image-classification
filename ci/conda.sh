4b33c05d09c644edc1e941826599048871ce4d08
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-20 16:03:12 +0100

    install conda if the user doesn't have conda installed

--- ci/conda.sh
+++ ci/conda.sh
@@ -46,6 +46,7 @@ if ! type conda > /dev/null; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   bash miniconda.sh -b -p "$HOME/.miniconda"
   export PATH="$HOME/.miniconda/bin:$PATH"
+  conda config --set always_yes yes
 fi
 
 # create conda environement
