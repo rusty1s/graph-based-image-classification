f08a69292a5e20d252193d71be9c4cc441dcecde
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-28 15:32:55 +0100

    from sh to bash

--- ci/install.sh
+++ ci/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p "$HOME/.miniconda"
@@ -6,4 +6,4 @@ rm -f miniconda.sh
 export PATH="$HOME/.miniconda/bin:$PATH"
 conda config --set always_yes yes
 
-. bin/install.sh -n test --python "$TRAVIS_PYTHON_VERSION" --tensorflow "$TENSORFLOW" --nauty "$NAUTY" --pynauty "$PYNAUTY"
+source bin/install.sh -n test --python "$TRAVIS_PYTHON_VERSION" --tensorflow "$TENSORFLOW" --nauty "$NAUTY" --pynauty "$PYNAUTY"
