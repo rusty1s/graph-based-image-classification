2941e6a42b8b0d828f636a4251853b6729ec9882
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-10 14:36:53 +0100

    uncomment tests

--- superpixels/save_test.py
+++ superpixels/save_test.py
@@ -49,31 +49,31 @@ def test_save_superpixel_tree_image():
     os.remove('./superpixels/tree-SLIC.jpg')
 
 
-# def test_save_superpixels():
-#     image = cv2.imread('./superpixels/test.png')
-#     assert_is_not_none(image)
+def test_save_superpixels():
+    image = cv2.imread('./superpixels/test.png')
+    assert_is_not_none(image)
 
-#     superpixels = extract_superpixels(image, image_to_slic_zero(image, 4))
+    superpixels = extract_superpixels(image, image_to_slic_zero(image, 4))
 
-#     save_superpixels(superpixels, '.', 'test.pkl')
+    save_superpixels(superpixels, '.', 'test.pkl')
 
-#     assert_true(os.path.exists('./test.pkl'))
+    assert_true(os.path.exists('./test.pkl'))
 
-#     os.remove('./test.pkl')
+    os.remove('./test.pkl')
 
 
-# def test_load_superpixels():
-#     image = cv2.imread('./superpixels/test.png')
-#     assert_is_not_none(image)
+def test_load_superpixels():
+    image = cv2.imread('./superpixels/test.png')
+    assert_is_not_none(image)
 
-#     superpixels = extract_superpixels(image, image_to_slic_zero(image, 4))
-#     assert_equals(len(superpixels), 4)
+    superpixels = extract_superpixels(image, image_to_slic_zero(image, 4))
+    assert_equals(len(superpixels), 4)
 
-#     save_superpixels(superpixels, '.', 'test.pkl')
+    save_superpixels(superpixels, '.', 'test.pkl')
 
-#     assert_true(os.path.exists('./test.pkl'))
+    assert_true(os.path.exists('./test.pkl'))
 
-#     loaded_superpixels = read_superpixels('./test.pkl')
-#     assert_equals(len(loaded_superpixels), 4)
+    loaded_superpixels = read_superpixels('./test.pkl')
+    assert_equals(len(loaded_superpixels), 4)
 
-#     os.remove('./test.pkl')
+    os.remove('./test.pkl')
