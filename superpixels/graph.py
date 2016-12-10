e3fe94bdcc0ac24a1b044ea62f53c43cb0da58dd
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-10 20:35:12 +0100

    python 2.6 super __init__ call

--- superpixels/graph.py
+++ superpixels/graph.py
@@ -3,7 +3,7 @@ from networkx import Graph
 
 class SuperpixelGraph(Graph):
     def __init__(self, superpixels, node_mapping, edge_mapping):
-        super().__init__()
+        super(SuperpixelGraph, self).__init__()
 
         for s in superpixels:
             self.add_node(s.id, node_mapping(s))
