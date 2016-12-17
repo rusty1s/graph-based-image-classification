bd3c6a835a3bfa110255c5de1c7e2618d045f226
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-17 17:02:28 +0100

    first graph training with next batch impl

--- train_patchy.py
+++ train_patchy.py
@@ -1,45 +1,40 @@
+import os
 import tensorflow as tf
 # import numpy as np
 # import os
 
-# from model import convolutional_1d
-from cifar10 import Cifar10
+from model import convolutional_1d
 from data import DataSet
 
-# Load the test data.
-# PATH_DATA = './datasets/cifar10/slic_zero/2016-12-12T20-22-40'
-# PATH_LABELS = './datasets/cifar10'
+# Load the correct pickle implementation for different python versions.
+try:
+    import cPickle as pickle
+except:
+    import _pickle as pickle
 
-cifar10 = Cifar10('./datasets/cifar10')
-cifar10.get_train_batch(0)
+# Load the test data.
+PATH_DATA = './datasets/cifar10/slic_zero/2016-12-17T15-22-11'
+PATH_LABELS = './datasets/cifar10'
+
+
+with open(os.path.join(PATH_DATA, 'data_batch_1'), 'rb') as f:
+    batch_1 = pickle.load(f)
+with open(os.path.join(PATH_DATA, 'data_batch_2'), 'rb') as f:
+    batch_2 = pickle.load(f)
+with open(os.path.join(PATH_DATA, 'data_batch_3'), 'rb') as f:
+    batch_3 = pickle.load(f)
+with open(os.path.join(PATH_DATA, 'data_batch_4'), 'rb') as f:
+    batch_4 = pickle.load(f)
+with open(os.path.join(PATH_DATA, 'data_batch_5'), 'rb') as f:
+    batch_5 = pickle.load(f)
 
 data = DataSet([
-    cifar10.get_train_batch(0),
-    cifar10.get_train_batch(1),
-    cifar10.get_train_batch(2),
-    cifar10.get_train_batch(3),
-    cifar10.get_train_batch(4),
+    batch_1,
+    batch_2,
+    batch_3,
+    batch_4,
+    batch_5,
 ])
-
-images, labels = data.next_batch(2)
-
-print(images)
-print(labels)
-print(images.shape)
-print(labels.shape)
-
-
-# def get_data_and_labels(batch):
-#     with open(os.path.join(PATH_DATA,
-#               'data_batch_{}'.format(batch+1)), 'rb') as input:
-#         data = pickle.load(input)
-
-#     with open(os.path.join(PATH_LABELS,
-#               'data_batch_{}'.format(batch+1)), 'rb') as input:
-#         labels = pickle.load(input, encoding='latin1')['labels']
-
-#     return data, labels
-
 
 # Build the model.
 NEIGHBORHOOD_SIZE = 9
@@ -69,47 +64,35 @@ STRUCTURE = {
     'out': 10,
 }
 
-# y_conv, x, y, keep_prob = convolutional_1d(STRUCTURE)
+y_conv, x, y, keep_prob = convolutional_1d(STRUCTURE)
 
-# # **Train the Model**
-# #
-# # To train and evaluate the model we will use the more sphisticated ADAM
-# # optimizer instead of the gradient descent optimizer.
-# softmax = tf.nn.softmax_cross_entropy_with_logits(y_conv, y)
-# cross_entropy = tf.reduce_mean(softmax)
+# **Train the Model**
+#
+# To train and evaluate the model we will use the more sphisticated ADAM
+# optimizer instead of the gradient descent optimizer.
+softmax = tf.nn.softmax_cross_entropy_with_logits(y_conv, y)
+cross_entropy = tf.reduce_mean(softmax)
 
-# LEARNING_RATE = 1e-4
-# train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+LEARNING_RATE = 1e-4
+train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
 
-# # **Evaluate the Model**
-# correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# **Evaluate the Model**
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# init = tf.initialize_all_variables()
+init = tf.initialize_all_variables()
 
-# with tf.Session() as sess:
-#     sess.run(init)
+print('START TRAINING')
 
-#     print('Training', 60000//50, 'batches')
+with tf.Session() as sess:
+    sess.run(init)
 
-#     for z in range(5):
-#         data, labels = get_data_and_labels(z)
+    for i in range(20000):
+        d, labels = data.next_batch(50)
+        train_step.run(feed_dict={x: d, y: labels, keep_prob: 0.5})
 
-#         # for i in range(10000//50):
-#         for i in range(1):
-#             data_batch = data[i*50:(i+1)*50]
-#             labels_batch = labels[i*50:(i+1)*50]
-
-#             new_labels = np.zeros((50, 10))
-#             for j in range(len(labels_batch)):
-#                 new_labels[j][labels_batch[j]] = 1.0
-#             labels_batch = new_labels
-
-#             train_step.run(feed_dict={x: data_batch, y: labels_batch,
-#                                       keep_prob: 0.5})
-
-#             if i % 5 == 0:
-#                 train_accuracy = accuracy.eval(feed_dict={x: data_batch,
-#                                                           y: labels_batch,
-#                                                           keep_prob: 1.0})
-#                 print('Step', z*200+i, 'Training accuracy', train_accuracy)
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={x: d,
+                                                      y: labels,
+                                                      keep_prob: 1.0})
+            print('Step', i, 'Training accuracy', train_accuracy)
