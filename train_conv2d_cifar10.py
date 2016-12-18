8789f4c15d4335328f753fdf9d49ce78ef73c57d
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-18 21:12:10 +0100

    fixed bug of rename load_data to load_dataset

--- train_conv2d_cifar10.py
+++ train_conv2d_cifar10.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from model import convolutional_2d
-from data import load_data
+from data import load_dataset
 
 # Load the correct pickle implementation for different python versions.
 try:
@@ -65,7 +65,7 @@ y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv,
                                                                        y_))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
