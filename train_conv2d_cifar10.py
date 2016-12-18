e50ecf5c78137bc986d79cdc26a255ee8d0c6fed
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-18 21:13:03 +0100

    change learning rate

--- train_conv2d_cifar10.py
+++ train_conv2d_cifar10.py
@@ -65,7 +65,7 @@ y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv,
                                                                        y_))
-train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
