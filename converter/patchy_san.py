61d2113198b01124170c96a408ee597fb2edfa63
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-28 17:40:25 +0100

    planing

--- converter/patchy_san.py
+++ converter/patchy_san.py
@@ -4,49 +4,11 @@ import numpy as np
 
 from .converter import Converter
 
-# from patchy import (receptive_fields, betweenness_centrality, order)
-# from superpixels import (image_to_slic_zero, extract_superpixels,
-#                          create_superpixel_graph)
-# from superpixel.algorithm import (slic, slico)
-
-
-# def convert_image_to_field(image):
-#     rep = image_to_slic_zero(image, 100)
-#     superpixels = extract_superpixels(image, rep)
-
-#     graph = create_superpixel_graph(superpixels, node_mapping, edge_mapping)
-#    fields = receptive_fields(graph, order, 1, 100, 10,
-#    betweenness_centrality,
-#                               node_features, 8)
-#     fields = fields.astype(np.float32)
-#     return fields
-
-
-# def node_mapping(superpixel):
-#     return superpixel.get_attributes()
-
-
-# def edge_mapping(from_superpixel, to_superpixel):
-#     return {}
-
-
-# def node_features(node_attributes):
-#     return [
-#         node_attributes['red'],
-#         node_attributes['green'],
-#         node_attributes['blue'],
-#         node_attributes['count'],
-#         node_attributes['y'],
-#         node_attributes['x'],
-#         node_attributes['height'],
-#         node_attributes['width'],
-#     ]
-
 
 class PatchySan(Converter):
 
     def __init__(self, grapher, num_nodes, node_labeling, node_stride,
-                 num_neighborhood, neighborhood_labeling, num_node_channels):
+                 neighborhood_size, neighborhood_labeling, num_node_channels):
 
         # TODO throw errors if labelings not in labelings dict
 
@@ -54,7 +16,7 @@ class PatchySan(Converter):
         self._num_nodes = num_nodes
         self._node_labeling = node_labeling
         self._node_stride = node_stride
-        self._num_neighborhood = num_neighborhood
+        self._neighborhood_size = neighborhood_size
         self._neighborhood_labeling = neighborhood_labeling
         self._num_node_channels = num_node_channels
 
@@ -62,7 +24,7 @@ class PatchySan(Converter):
     def shape(self):
         return [
             self._num_nodes,
-            self._num_neighborhood,
+            self._neighborhood_size,
             self._num_node_channels
         ]
 
@@ -72,47 +34,26 @@ class PatchySan(Converter):
             'num_nodes': self._num_nodes,
             'node_labeling': self._node_labeling,
             'node_stride': self._node_stride,
-            'num_neighborhood': self._num_neighborhood,
+            'neighborhood_size': self._neighborhood_size,
             'neighborhood_labeling': self._neighborhood_labeling,
             'num_node_channels': self._num_node_channels,
         }
 
-    def convert(self, image):
-        graph = self._grapher.create_graph(image)
+    def convert(self, data):
+        nodes, adjacent = self._grapher.create_graph(data)
 
-        sequence = labelings[self._node_labeling](graph)
+        sequence = labelings[self._node_labeling](adjacent)
         sequence = node_sequence(sequence, self._num_nodes, self._node_stride)
-        # s = slico(image, 100)
-        # TODO to graph
-        # TODO to data
-        return image
-        # s = tf.reshape(s, [24, 24, 1])
-        # s = tf.cast(s, tf.float32)
-        # return s
+        neighborhoods = assemble_neighborhoods(
+            sequence, adjacent, self._neighborhood_size,
+            labelings[self._neighborhood_labeling])
 
-        # image = tf.cast(data, tf.int32)
-        # field = tf.py_func(convert_image_to_field, [image], tf.float32,
-        #                    stateful=False, name='GRAPH')
-        # return field
+        # TODO receptive fields
+        # this is relative easy, all we need to do is create another mapper
+        # function that replaces itself with the corresponding node features or
+        # with zero vectors if value is -1.
 
-
-def node_sequence(sequence, width, stride):
-    # Stride the sequence based on the given stride width.
-    size = sequence.get_shape()[0].value
-    sequence = tf.strided_slice(sequence, [0], [size], [stride])
-
-    # No more entries than we want.
-    sequence = tf.strided_slice(sequence, [0], [width], [1])
-
-    # Pad with zeros if we need to.
-    size = sequence.get_shape()[0].value
-
-    if size < width:  # TODO if weg, muss auch ohne gehen
-        sequence = tf.add(sequence, tf.ones_like(sequence))
-        sequence = tf.pad(sequence, [[0, width-size]])
-        sequence = tf.sub(sequence, tf.ones_like(sequence))
-
-    return sequence
+        return data
 
 
 def betweenness_centrality(adjacent):
@@ -134,3 +75,41 @@ def betweenness_centrality(adjacent):
 labelings = {
     'betweenness_centrality': betweenness_centrality,
 }
+
+
+def node_sequence(sequence, width, stride):
+    # Stride the sequence based on the given stride width.
+    size = sequence.get_shape()[0].value
+    sequence = tf.strided_slice(sequence, [0], [size], [stride])
+
+    # No more entries than we want.
+    sequence = tf.strided_slice(sequence, [0], [width], [1])
+
+    # Pad with zeros if we need to.
+    size = sequence.get_shape()[0].value
+
+    if size < width:  # TODO if weg, muss auch ohne gehen
+        sequence = tf.add(sequence, tf.ones_like(sequence))
+        sequence = tf.pad(sequence, [[0, width-size]])
+        sequence = tf.sub(sequence, tf.ones_like(sequence))
+
+    return sequence
+
+
+def assemble_neighborhoods(sequence, adjacent, size, labeling):
+    assemble = assemble_neighborhood(size, adjacent)
+
+    sequence = tf.reshape([-1, 1])
+    neighborhoods = tf.map_fn(assemble, t)
+
+    return neighborhoods
+
+
+def assemble_neighborhood(index, adjacent, size, labeling):
+    # this shall return a function assemble(index) with a fixed size
+    # this is neighborhood assembly where we already compute a fixed size
+    # neighborhood via labeling and distances
+    def _assemble(index):
+        return index
+
+    return _assemble
