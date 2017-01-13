f2af80c9a00882f9e5b0d643e863130a6fbd384f
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-13 11:03:26 +0100

    clean up inputs code

--- data/inputs.py
+++ data/inputs.py
@@ -9,30 +9,31 @@ MIN_FRACTION_OF_EXAMPLES_IN_QUEUE = 0.4
 NUM_THREADS = 16
 
 
-def inputs(dataset, batch_size=BATCH_SIZE, train=True, eval_data=False):
+def inputs(dataset, distort_inputs=True, batch_size=BATCH_SIZE,
+           num_epochs=None, shuffle=True, eval_data=False):
     """Constructs inputs from a dataset.
 
     Args:
         dataset: Instance of the dataset to use.
-        batch_size: Number of data per batch.
-        train: Boolean indiciating if one wants to train or evaluate. Default:
-          True.
+        distort_inputs: Boolean whether to distort the inputs (optional).
+        batch_size: Number of data per batch (optional).
+        num_epochs: Number indicating the maximal number of epochs iterations
+          before raising an OutOfRange error (optional).
+        shuffle: Boolean indiciating if one wants to shuffle the inputs
+          (optional).
         eval_data: Boolean indicating if one should use the train or eval data
-          set. Default: False.
+          set (optional).
 
     Returns:
         data_batch: 4D tensor of [batch_size, height, width, depth] size.
         label_batch: 1D tensor of [batch_size] size.
     """
 
-    # When training, we want to apply a different distortion as if we are
-    # evaluating.
-    if train:
+    # When shuffling one wants to also apply a random distortion.
+    if shuffle:
         distort = distort_image_for_train
-        shuffle = True
     else:
         distort = distort_image_for_eval
-        shuffle = False
 
     # Choose the right dataset.
     if not eval_data:
@@ -42,37 +43,16 @@ def inputs(dataset, batch_size=BATCH_SIZE, train=True, eval_data=False):
         filenames = dataset.eval_filenames
         num_examples_per_epoch = dataset.num_examples_per_epoch_for_eval
 
-    # Construct the inputs.
-    return _inputs(filenames, dataset.read, distort, num_examples_per_epoch,
-                   batch_size, shuffle)
-
-
-def _inputs(filenames, read, distort, num_examples_per_epoch, batch_size,
-            shuffle):
-
-    """Constructs inputs from data files using the read operation.
-
-    Args:
-        filenames: The filenames of the data batches in the data directory.
-        read: Reader operation that returns a single record.
-        distort: Distort operation on the data of the example.
-        num_examples_per_epoch: Number of examples per epoch.
-        batch_size: Number of data per batch.
-        shuffle: Boolean indicating whether to use a shuffling queue.
-
-    Returns:
-        data_batch: 4D tensor of [batch_size, height, width, depth] size.
-        label_batch: 1D tensor of [batch_size] size.
-    """
-
     # Create a queue that produces the filenames to read.
-    filename_queue = tf.train.string_input_producer(filenames)
+    filename_queue = tf.train.string_input_producer(
+        filenames, num_epochs, shuffle)
 
     # Read examples from files in the filename queue.
-    record = read(filename_queue)
+    record = dataset.read(filename_queue)
 
     # Distort the data.
-    record = distort(record)
+    if distort_inputs:
+        record = distort(record)
 
     min_queue_examples = int(num_examples_per_epoch *
                              MIN_FRACTION_OF_EXAMPLES_IN_QUEUE)
