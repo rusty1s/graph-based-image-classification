d754824edf13353b25156b9e485ef5f2520d231f
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-16 11:09:41 +0100

    duplication removed

--- superpixel/algorithm/slic.py
+++ superpixel/algorithm/slic.py
@@ -7,36 +7,32 @@ MAX_ITERATIONS = 10
 SIGMA = 0.0
 
 
-def slic(image, num_superpixels, compactness=COMPACTNESS,
-         max_iterations=MAX_ITERATIONS, sigma=SIGMA):
+def _slic(image, slic_zero, num_superpixels, compactness=COMPACTNESS,
+          max_iterations=MAX_ITERATIONS, sigma=SIGMA):
 
     def _slic_image(image):
         superpixels = _slic(image, n_segments=num_superpixels,
                             compactness=compactness, max_iter=max_iterations,
-                            sigma=sigma, slic_zero=False)
+                            sigma=sigma, slic_zero=slic_zero)
         # py_func expects a float as out type.
         return superpixels.astype(np.float32)
 
-    image = tf.cast(image, tf.int32)
+    image = tf.cast(image, tf.uint8)
     superpixels = tf.py_func(_slic_image, [image], tf.float32, stateful=False,
-                             name='slic')
+                             name='slico' if slic_zero else 'slic')
     return tf.cast(superpixels, tf.int32)
+
+
+def slic(image, num_superpixels, compactness=COMPACTNESS,
+         max_iterations=MAX_ITERATIONS, sigma=SIGMA):
+    return _slic(
+        image, True, num_superpixels, compactness, max_iterations, sigma)
 
 
 def slico(image, num_superpixels, compactness=COMPACTNESS,
           max_iterations=MAX_ITERATIONS, sigma=SIGMA):
-
-    def _slico_image(image):
-        superpixels = _slic(image, n_segments=num_superpixels,
-                            compactness=compactness, max_iter=max_iterations,
-                            sigma=sigma, slic_zero=True)
-        # py_func expects a float as out type
-        return superpixels.astype(np.float32)
-
-    image = tf.cast(image, tf.int32)
-    superpixels = tf.py_func(_slico_image, [image], tf.float32, stateful=False,
-                             name='slico')
-    return tf.cast(superpixels, tf.int32)
+    return _slic(
+        image, False, num_superpixels, compactness, max_iterations, sigma)
 
 
 def slic_generator(num_superpixels, compactness=COMPACTNESS,
