1dddbe0bca9e2bf9b7f2b7b5ba7f477a4e34d13e
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-21 13:58:06 +0100

    beginning of feature collection

--- superpixel/features.py
+++ superpixel/features.py
@@ -3,40 +3,32 @@ import numpy as np
 from skimage.measure import regionprops
 
 
-# NACHLESEN:
-# Equivalent diameter WAS IST DAS?
-# Euler number BRAUCHE ICH NICHT DA KEINE LOECHER???
+NUM_FEATURES = 5
 
 
-def segmentation_features(segmentation):
-    def _segmentation_features(segmentation):
-        props = regionprops(segmentation)
+def features(segmentation, image):
+    def _features(segmentation, intensity_image):
+        props = regionprops(segmentation, intensity_image)
+        f = np.zeros((len(props), NUM_FEATURES), dtype=np.float32)
+
         for i, prop in enumerate(props):
-            print('----', i, '----')
-            print('area', prop['area'])
-            print('bbox', prop['bbox'])
-            print('centroid', prop['local_centroid'])
-            print('convex_area', prop['convex_area'])
-            print('eccentricity', prop['eccentricity'])
-            print('equivalent_diameter', prop['equivalent_diameter'])
-            print('euler_number', prop['euler_number'])
-            print('extent', prop['extent'])
-            print('filled_area', prop['filled_area'])
-            print('major_axis_length', prop['major_axis_length'])
-            print('minor_axis_length', prop['minor_axis_length'])
-            print('solidity', prop['solidity'])
-            print('filled_area', prop['filled_area'])
-            print('perimeter', prop['perimeter'])
+            pass
 
-            # print('moments', prop['moments']) liefert M00 bis M33
-            # print('central moments', prop['moments_central'])
-
-        return np.zeros((0, 0), dtype=np.float32)
-        # return tf.zeros([2, 2], dtype=tf.float32)
+        return f
 
     # We need to increment the segmentation, because labels with value 0 are
     # ignored when calling regionprops.
     segmentation = segmentation + tf.ones_like(segmentation)
 
-    return tf.py_func(_segmentation_features, [segmentation], tf.float32,
+    # Get the intensity image with shape [height, width] of the image.
+    with tf.name_scope('intensity_image', values=[image]):
+        image = tf.cast(image, dtype=tf.uint8)
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = tf.image.rgb_to_hsv(image)
+        image = tf.strided_slice(image, [0, 0, 2],
+                                 [tf.shape(image)[0], tf.shape(image)[1], 3],
+                                 [1, 1, 1])
+        image = tf.squeeze(image)
+
+    return tf.py_func(_features, [segmentation, image], tf.float32,
                       stateful=False, name='segmentation_features')
--- superpixel/features_test.py
+++ superpixel/features_test.py
@@ -1,17 +1,24 @@
 import tensorflow as tf
 
-from .features import segmentation_features
+from .features import features
 
 
 class FeaturesTest(tf.test.TestCase):
 
     def test_features(self):
+        image = tf.constant([
+            [[255, 255, 255], [255, 255, 255], [0, 0, 0], [0, 0, 0]],
+            [[255, 255, 255], [255, 255, 255], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [255, 255, 255], [255, 255, 255]],
+            [[0, 0, 0], [0, 0, 0], [255, 255, 255], [255, 255, 255]],
+        ])
+
         segmentation = tf.constant([
-            [1, 1, 0, 0],
-            [1, 0, 0, 1],
             [0, 0, 0, 0],
-            [0, 1, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+            [0, 2, 2, 2],
         ], dtype=tf.int32)
 
         with self.test_session() as sess:
-            print(segmentation_features(segmentation).eval())
+            print(features(segmentation, image).eval())
