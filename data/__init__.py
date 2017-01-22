ecfc5266ee706e1b43e7d1a425abb33f06a04c6f
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-22 16:37:00 +0100

    distort images dont need to be public

--- data/__init__.py
+++ data/__init__.py
@@ -7,8 +7,6 @@ from .pascal_voc import PascalVOC
 from .helper.inputs import inputs
 from .helper.iterator import iterator
 from .helper.tfrecord import read_tfrecord, write_to_tfrecord
-from .helper.distort_image import distort_image_for_train,\
-                                  distort_image_for_eval
 
 datasets = {'cifar_10': Cifar10,
             'pascal_voc': PascalVOC}
