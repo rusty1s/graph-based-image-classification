504c4da4d1a33357494435afffde4913a8793e70
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-28 13:54:52 +0100

    bugfixes

--- patchy/__init__.py
+++ patchy/__init__.py
@@ -1,8 +1,9 @@
 from .patchy import PatchySan
 
 from .helper.labeling import labelings,\
-                             identity,\
+                             scanline,\
                              betweenness_centrality
 
 from .helper.neighborhood_assembly import neighborhood_assemblies,\
-                                          neighborhoods_by_weight
+                                          neighborhoods_weights_to_root,\
+                                          neighborhoods_nearest_scanline
