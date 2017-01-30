5677efed044bc977b7b477c1f79d7920b7e914eb
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-30 08:26:28 +0100

    reworked feature extraction + tests

--- segmentation/feature_extraction.py
+++ segmentation/feature_extraction.py
@@ -6,7 +6,7 @@ from skimage.measure import regionprops
 
 
 # Static number of features to generate for one segment.
-NUM_FEATURES = 81
+NUM_FEATURES = 45
 
 
 # TODO Oriented Bounding Box missing
@@ -30,132 +30,36 @@ def feature_extraction(segmentation, image):
         features = np.zeros((len(props), NUM_FEATURES), dtype=np.float32)
 
         for i, prop in enumerate(props):
-            area = prop['area']
-            features[i][0] = area
+            # Moments features.
+            features[i][0:16] = prop['moments'].flatten()
 
+            # Bounding box features.
             bbox = prop['bbox']
-            features[i][1] = bbox[2] - bbox[0]  # bbox height
-            features[i][2] = bbox[3] - bbox[1]  # bbox height
+            features[i][16] = bbox[2] - bbox[0]
+            features[i][17] = bbox[3] - bbox[1]
 
-            features[i][3] = prop['convex_area']
-            features[i][4] = prop['eccentricity']
-            features[i][5] = prop['equivalent_diameter']
-            features[i][6] = prop['euler_number']
-            features[i][7] = prop['extent']
-            features[i][8] = prop['filled_area']
+            # Polygon features.
+            features[i][18] = prop['convex_area']
+            features[i][19] = prop['perimeter']
 
-            inertia_tensor = prop['inertia_tensor']
-            features[i][9] = inertia_tensor[0][0]  # mu'_20
-            features[i][10] = inertia_tensor[0][1]  # mu'_11
-            features[i][11] = inertia_tensor[1][1]  # mu'_02
+            # Weighted moments features.
+            features[i][20:36] = prop['weighted_moments'].flatten()
 
-            inertia_tensor_eigvals = prop['inertia_tensor_eigvals']
-            features[i][12] = inertia_tensor_eigvals[0]  # lambda_1
-            features[i][13] = inertia_tensor_eigvals[1]  # lambda_2
-
-            local_centroid = prop['local_centroid']
-            features[i][14] = local_centroid[0]
-            features[i][15] = local_centroid[1]
-
-            features[i][16] = prop['major_axis_length']
-            features[i][17] = prop['minor_axis_length']
-
-            features[i][18] = prop['max_intensity']
-            features[i][19] = prop['mean_intensity']
-            features[i][20] = prop['min_intensity']
-
-            mu = prop['moments_central']
-            features[i][21] = mu[0][0]
-            features[i][22] = mu[1][1]
-            features[i][23] = mu[2][0]
-            features[i][24] = mu[0][2]
-            features[i][25] = mu[2][1]
-            features[i][26] = mu[1][2]
-            features[i][27] = mu[2][2]
-
-            eta = prop['moments_normalized']
-            features[i][28] = eta[1][1]
-            features[i][29] = eta[2][0]
-            features[i][30] = eta[0][2]
-            features[i][31] = eta[2][1]
-            features[i][32] = eta[1][2]
-            features[i][33] = eta[2][2]
-
-            hu = prop['moments_hu']
-            features[i][34] = hu[0]
-            features[i][35] = hu[1]
-            features[i][36] = hu[2]
-            features[i][37] = hu[3]
-            features[i][38] = hu[4]
-            features[i][39] = hu[5]
-            features[i][40] = hu[6]
-
-            perimeter = prop['perimeter']
-            features[i][41] = perimeter
-
-            features[i][42] = prop['orientation']
-            features[i][43] = prop['solidity']
-
-            # TODO a few pictures haw intensity images of all zeros, so these
-            # operations results in nans.
-            wlc = prop['weighted_local_centroid']
-            features[i][44] = wlc[0] if not isnan(wlc[0]) else 0.0
-            features[i][45] = wlc[1] if not isnan(wlc[1]) else 0.0
-
-            wm = prop['weighted_moments_central']
-            features[i][46] = wm[0][0] if not isnan(wm[0][0]) else 0.0
-            features[i][47] = wm[1][1] if not isnan(wm[1][1]) else 0.0
-            features[i][48] = wm[2][0] if not isnan(wm[2][0]) else 0.0
-            features[i][49] = wm[0][2] if not isnan(wm[0][2]) else 0.0
-            features[i][50] = wm[2][1] if not isnan(wm[2][1]) else 0.0
-            features[i][51] = wm[1][2] if not isnan(wm[1][2]) else 0.0
-            features[i][52] = wm[2][2] if not isnan(wm[2][2]) else 0.0
-
-            we = prop['weighted_moments_normalized']
-            features[i][53] = we[1][1] if not isnan(we[1][1]) else 0.0
-            features[i][54] = we[2][0] if not isnan(we[2][0]) else 0.0
-            features[i][55] = we[0][2] if not isnan(we[0][2]) else 0.0
-            features[i][56] = we[2][1] if not isnan(we[2][1]) else 0.0
-            features[i][57] = we[1][2] if not isnan(we[1][2]) else 0.0
-            features[i][58] = we[2][2] if not isnan(we[2][2]) else 0.0
-
-            wh = prop['moments_hu']
-            features[i][59] = wh[0] if not isnan(wh[0]) else 0.0
-            features[i][60] = wh[1] if not isnan(wh[1]) else 0.0
-            features[i][61] = wh[2] if not isnan(wh[2]) else 0.0
-            features[i][62] = wh[3] if not isnan(wh[3]) else 0.0
-            features[i][63] = wh[4] if not isnan(wh[4]) else 0.0
-            features[i][64] = wh[5] if not isnan(wh[5]) else 0.0
-            features[i][65] = wh[6] if not isnan(wh[6]) else 0.0
-
-            circularity = (4 * PI * area) / (perimeter**2)
-            features[i][67] = circularity
-
-            central_moment_feature = (mu[2][0] + mu[0][2]) / mu[0][0]
-            features[i][67] = central_moment_feature
-
-            elongation = (sqrt((mu[2][0] - mu[0][2])**2 + mu[1][1]**2)) /\
-                         (mu[2][0] + mu[0][2])
-            features[i][68] = elongation
-
+            # Color features.
             sliced_image = image[bbox[0]:bbox[2], bbox[1]:bbox[3]]
             sliced_image = sliced_image[prop['image']]
 
-            features[i][69] = sliced_image[..., 0].mean()
-            features[i][70] = sliced_image[..., 1].mean()
-            features[i][71] = sliced_image[..., 2].mean()
+            features[i][36] = sliced_image[..., 0].mean()
+            features[i][37] = sliced_image[..., 1].mean()
+            features[i][38] = sliced_image[..., 2].mean()
 
-            features[i][72] = sliced_image[..., 0].sum()
-            features[i][73] = sliced_image[..., 1].sum()
-            features[i][74] = sliced_image[..., 2].sum()
+            features[i][39] = sliced_image[..., 0].min()
+            features[i][40] = sliced_image[..., 1].min()
+            features[i][41] = sliced_image[..., 2].min()
 
-            features[i][75] = sliced_image[..., 0].min()
-            features[i][76] = sliced_image[..., 1].min()
-            features[i][77] = sliced_image[..., 2].min()
-
-            features[i][78] = sliced_image[..., 0].max()
-            features[i][79] = sliced_image[..., 1].max()
-            features[i][80] = sliced_image[..., 2].max()
+            features[i][42] = sliced_image[..., 0].max()
+            features[i][43] = sliced_image[..., 1].max()
+            features[i][44] = sliced_image[..., 2].max()
 
         return features
 
--- segmentation/feature_extraction_test.py
+++ segmentation/feature_extraction_test.py
@@ -1,3 +1,5 @@
+from math import sqrt, pi as PI
+
 import tensorflow as tf
 
 from .feature_extraction import feature_extraction
@@ -7,18 +9,49 @@ class FeaturesTest(tf.test.TestCase):
 
     def test_features(self):
         image = tf.constant([
-            [[255, 255, 255], [255, 255, 255], [0, 0, 0], [0, 0, 0]],
-            [[255, 255, 255], [255, 255, 255], [0, 0, 0], [0, 0, 0]],
-            [[0, 0, 0], [0, 0, 0], [255, 255, 255], [255, 255, 255]],
-            [[0, 0, 0], [0, 0, 0], [255, 255, 255], [255, 255, 255]],
+            [[255, 0,   0],   [0,   0,   0],   [0,   0,   0]],
+            [[0,   0,   0],   [0,   0,   0],   [255, 255, 255]],
+            [[255, 255, 255], [255, 255, 255], [255, 255, 255]],
         ])
 
         segmentation = tf.constant([
-            [0, 0, 0, 0],
-            [0, 0, 1, 1],
-            [0, 0, 0, 1],
-            [0, 2, 2, 2],
+            [0, 0, 0],
+            [0, 0, 1],
+            [1, 1, 1],
         ], dtype=tf.int32)
 
+        M = [[5, 2, 2, 2],
+             [4, 1, 1, 1],
+             [6, 1, 1, 1],
+             [10, 1, 1, 1]]
+
+        bbox = [2, 3]
+        convex_area = 5
+        perimeter = 4 + (1 + sqrt(2)) / 2
+
+        Mw = [[1, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0]]
+
+        mean = [0.2, 0, 0]
+        minimum = [0, 0, 0]
+        maximum = [1, 0, 0]
+
         with self.test_session() as sess:
-            print(feature_extraction(segmentation, image).eval())
+            f = feature_extraction(segmentation, image).eval()[0]
+            self.assertAllEqual(f[0:4], M[0])
+            self.assertAllEqual(f[4:8], M[1])
+            self.assertAllEqual(f[8:12], M[2])
+            self.assertAllEqual(f[12:16], M[3])
+            self.assertAllEqual(f[16:18], bbox)
+            self.assertEqual(f[18], convex_area)
+            self.assertEqual(round(f[19]*100), round(perimeter*100))
+            self.assertAllEqual(f[20:24], Mw[0])
+            self.assertAllEqual(f[24:28], Mw[1])
+            self.assertAllEqual(f[28:32], Mw[2])
+            self.assertAllEqual(f[32:36], Mw[3])
+            self.assertAllEqual(round(f[36]*100), round(mean[0]*100))
+            self.assertAllEqual(f[37:39], mean[1:3])
+            self.assertAllEqual(f[39:42], minimum)
+            self.assertAllEqual(f[42:45], maximum)
